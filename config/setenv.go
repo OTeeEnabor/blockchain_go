e1a649b6027d6399488400a4850c997751bf1380
unknown <tobienabor@gmail.com>
2023-09-13 18:57:07 +0200

    changed the config file path settings

--- config/setenv.go
+++ config/setenv.go
@@ -1,6 +1,24 @@
 package config
 
-// set file name
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func getConfigFile() string {
+	// set file name
+	path, _ := os.Executable()
+	// get file path
+	filePath := filepath.Dir(path)
+	// store  template folder path into variable (CLOUD server)
+	configFile := fmt.Sprintf("%v/config/production.yaml",filePath)
+	
+	return configFile
+}
+
 
 // var EnVar = GetConfig("development")
-var EnVar = GetConfig("production")
+// set file name
+var configFilePath = getConfigFile()
+var EnVar = GetConfig(configFilePath)
