8d0bb4f79d0fb828135eb68f6c0f9bf5bdbb2e98
unknown <tobienabor@gmail.com>
2023-09-18 09:22:10 +0200

    modified the products model for the blockchain aspect

--- models/products.go
+++ models/products.go
@@ -12,4 +12,13 @@ type Products struct {
 	Colour    string
 	Quantity  int64
 	Timestamp time.Time
+	ContractId *ContractID
+	ContractIdString string
+	GasUsed uint64
+	TransactionId string
+	ChargeFee string
+	PayerAccount string
+	Status string
+	ReceiptPDF string
+	CreatedAt time.Time
 }
